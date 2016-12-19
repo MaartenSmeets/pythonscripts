ca06a82ff6c63f8581bafba060163e31f9c043d4
Maarten Smeets <MaartenSmeets@users.noreply.github.com>
2016-12-19 09:15:51 +0100

    Filtering and ordering results

--- weblogic/dms/weblogicdms.py
+++ weblogic/dms/weblogicdms.py
@@ -1,12 +1,17 @@
 from requests import session 
-import string  
+from operator import itemgetter
+import string 
+import os 
+import re
+import argparse
 import xml.etree.ElementTree as ET
 
-WEBLOGIC_HOST="localhost"
-WEBLOGIC_PORT="7101"  
-WEBLOGIC_USERNAME="weblogic"  
-WEBLOGIC_PASSWORD="Welcome01"  
-DEBUG='true'
+global args
+
+WEBLOGIC_HOST="hostname"
+WEBLOGIC_PORT="port"  
+WEBLOGIC_USERNAME="username"  
+WEBLOGIC_PASSWORD="password"  
 
 def get_xpath_nodes_from_tree(element,xpath):
     return element.findall(xpath)
@@ -18,14 +23,17 @@ def get_et_from_string(res):
     return ET.fromstring(res)
 
 def get_rows_from_tbml(tbml_et):
-    return get_xpath_nodes_from_tree(tbml_et,'./{http://www.oracle.com/AS/collector}table/{http://www.oracle.com/AS/collector}row')
+    return get_xpath_nodes_from_tree(tbml_et,'./table/row')
     
 def get_columns_from_row(row_et):
-    return get_xpath_nodes_from_tree(row_et,'./{http://www.oracle.com/AS/collector}column')
+    return get_xpath_nodes_from_tree(row_et,'./column')
 
 def get_name_value_from_column(column_et):
     return column_et.get('name'),column_et.text
-
+	
+def exceldecimal(s):                   # voor de excel gebruikers
+  return(re.sub('\.',',',s))          # decimale . naar ,
+  
 payload = {
      'j_username': WEBLOGIC_USERNAME,
      'j_password': WEBLOGIC_PASSWORD
@@ -38,30 +46,28 @@ s.post(baseurl+'/dms/j_security_check', data=payload)
 response = s.get(baseurl+'/dms/index.html?format=xml&cache=false&prefetch=false&table=wls_webservice_operation&orderby=Name')
 tbml_et = get_et_from_string(response.text)
 rows = get_rows_from_tbml(tbml_et)
-firstrow='true'
+result=[]
+
 for row in rows:
-    columns = get_columns_from_row(row)
-    if firstrow=='true':
-        firstrow='false'
-        firstcolumn='true'
-        for column in columns:
-            name,value = get_name_value_from_column(column)
-            if firstcolumn == 'true':
-                resstr = name
-                firstcolumn='false'
-            else:
-                resstr = resstr+';'+name
-        print resstr
-    firstcolumn='true'
-    resstr=''
-    for column in columns:
-        name,value = get_name_value_from_column(column)
-        if firstcolumn == 'true':
-            resstr = value
-            firstcolumn='false'
-        else:
-            resstr = resstr+';'+value
-    print resstr
+  columns = get_columns_from_row(row)
+  h,n,s,mint,maxt,avgt,comp='','','',0,0,0,0
+  for column in columns:
+    k,v = get_name_value_from_column(column)
+    if k == 'Host':                 h    = v
+    if k == 'Name':                 n    = v
+    if k == 'wls_ear':              s    = v  
+    if k == 'Invoke.minTime':       mint = exceldecimal(v)
+    if k == 'Invoke.maxTime':       maxt = exceldecimal(v)
+    if k == 'Invoke.avg':           avgt = exceldecimal(v)
+    if k == 'Invoke.completed':     comp = int(v)
 
+  if comp > 0:
+    result.append([h,n,s,mint,maxt,avgt,comp])
 
+result.sort(key=itemgetter(0))  #sorteren op wls_ear,name,host
+result.sort(key=itemgetter(1))
+result.sort(key=itemgetter(2))
 
+result.insert(0, ['Host','Operation','Service','MinTime','MaxTime','AvgTime','Calls'])
+for x in result:
+  print(';'.join(map(str,x)))
