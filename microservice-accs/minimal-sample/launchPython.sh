ec8fbab3d7b924742417100f06b8fbb6587a45a3
MaartenSmeets <wisp1980@gmail.com>
2018-03-04 21:08:55 +0100

    File verwijderen

--- microservice-accs/minimal-sample/launchPython.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-pip --no-cache-dir install -r python/requirements.txt -t ${PYTHONPATH}
-uname --help
-ls --help
-whereis sh
-pwd
-export FLASK_APP=${APP_HOME}/python/sample.py
-flask run --host=$HOST --port=$PORT
