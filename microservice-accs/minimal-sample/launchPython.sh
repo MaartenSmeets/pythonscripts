8898f344273a7a9de86ba6622818f464df7ea316
MaartenSmeets <wisp1980@gmail.com>
2018-03-05 11:48:03 +0100

    Working now

--- microservice-accs/minimal-sample/launchPython.sh
+++ microservice-accs/minimal-sample/launchPython.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-pip --no-cache-dir install -r python/requirements.txt -t ${PYTHONPATH}
-uname --help
-ls --help
-whereis sh
-pwd
-export FLASK_APP=${APP_HOME}/python/sample.py
-flask run --host=$HOST --port=$PORT
+echo Args: $@
+export PYTHONPATH=`pwd`/python/modules
+echo PYTHONPATH: $PYTHONPATH
+pip --no-cache-dir install -r python/requirements.txt -t $PYTHONPATH
+export FLASK_APP=$APP_HOME/python/sample.py
+echo FLASK_APP: $FLASK_APP
+ls -altr python
+python -m flask run --host=$1 --port=$2
